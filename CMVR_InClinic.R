e9b187301ffb08f33f1c3e56d578f15cd7ca7bbc
bmsnyder <32952761+bmsnyder@users.noreply.github.com>
2020-02-04 11:37:21 +0700

    Update CMVR_InClinic.R
    This update is from 02.04.2020 -
    The main things I am looking for clarification is next steps and if the combined dataset is complete and ready for analysis. If more data is needed we will need to get from Nakornping prior to leaving.

--- CMVR_InClinic.R
+++ CMVR_InClinic.R
@@ -4,13 +4,13 @@ library(dplyr)
 library(skimr)
 library(rlang)
 library(lubridate)
- 
+
 ##Import 
 
-inclinic_data <- read_csv("In-Clinic Screening Photographer Worksheet 4.4.16-JK2_10.1.csv")
+inclinic_data <- read_csv("In-Clinic Screening Photographer Worksheet 4.4.16-JK2_v2_10.1.19.csv")
 View(inclinic_data)
 
-cd4_data <- read_csv("CD4consolidated_updated_10.1.csv")
+cd4_data <- read_csv("CD4consolidated_updated_v2_10.1.19.csv")
 View(cd4_data)
 
 ##Review 
@@ -252,84 +252,89 @@ afterphotoperiod <-combineddata %>%
 
 ## Define a positive case for each dataset (getting exam by Choeng in non-photo, and photo in photo)
 
-##----- BMS stop here 
+##----- BMS stop here --- the below is old 
+## Ok I think that this is the correct combined and complete dataset, do you agree? 
 
-afterphotoperiod  <- afterphotoperiod %>%
-  mutate(phototaken = case_when(!is.na(photonumber) ~ 1,
-                                 is.na(photonumber) ~ 0))
+##? Now what? 
+##? Let me know any forward steps - the main question is do we have all the data that we need from Thailand?
+##? If so, should we wrap this up since we seem relatively close, or should we defer to complete analysis on CMU Screening (DR, AMD glaucoma) and CMV QOL cohort? 
+##? Let me know your thoughts
 
-#? I am unsure if the below defintion is correct, because I don't know the data well, 
-# but these patients all have a Y dor dr. p or Dr. N, so I am assuming this means exam
-
-beforephotoperiod  <- beforephotoperiod %>%
-  mutate(examperformed = case_when(include == "Y" ~ 1,
-                                   include == "N" ~ 0))
-
-## Returning to data:
-# my confusion is that it seems like "patientrefertochoeng" would be the right way to do this,
-# however this variable is missing for most cases in the before, 
-# maybe this is the point and I will define separately as refertochoeng
-
-beforephotoperiod  <- beforephotoperiod %>%
-  mutate(refertochoeng = case_when(patientrefertochoeng == "Yes" ~ 1,
-                                   TRUE ~ 0))
-
-##create column if CD4 is <100
-
-afterphotoperiod  <- afterphotoperiod %>%
-  mutate(cd4less100 = case_when(cd4_cell_ws <100 ~ 1,
-                                cd4_cell_ws >100 ~ 0))
-
-beforephotoperiod  <- beforephotoperiod %>%
-  mutate(cd4less100 = case_when(cd4_cell_ws <100 ~ 1,
-                                cd4_cell_ws >100 ~ 0))
-
-afterphotoperiod  <- afterphotoperiod %>%
-  mutate(cd4less100.2 = case_when(cd4count <100 ~ 1,
-                                cd4count >100 ~ 0))
-
-beforephotoperiod  <- beforephotoperiod %>%
-  mutate(cd4less100.2 = case_when(cd4count <100 ~ 1,
-                                cd4count >100 ~ 0))
-
-## Tally numbers in each group and divide by total 
-
-count(beforephotoperiod, examperformed)
-#348/486
-count(afterphotoperiod, phototaken)
-#228/595
-count(beforephotoperiod, cd4less100)
-#10/486
-count(afterphotoperiod, cd4less100)
-#21/595
-count(beforephotoperiod, cd4less100.2)
-#2/486 with 433 NA
-count(afterphotoperiod, cd4less100.2)
-#6/595 with 371 NA
-
-### Hmm, to my understanding this is the opposite of our hypothesis, correct?
-# I feel that I may not have made the definitions. Are these correct? 
-# I may stop here to clarify before moving forward...
-# Also the amount of participants with a cd4 count <100 is way smaller than what is listed in the .do file
-### Returning to data:
-count(beforephotoperiod, refertochoeng)
-#8/486
-
-## Ok, perhaps this is the real number ~2% before camera to ~33% after camera, this seems right now, correct?
-##Let's look at % with cd4 <100
-
-beforecd4less100 <- beforephotoperiod %>%
-  filter(cd4less100 ==1)
-
-count(beforecd4less100, refertochoeng)
-# 0/10
-
-aftercd4less100 <- afterphotoperiod %>%
-  filter(cd4less100 ==1)
-
-count(aftercd4less100, phototaken)
-# 7/21
-
-#From 0 to 33% in this group as well. 
-
-## Let me know any forward steps
+# 
+# afterphotoperiod  <- afterphotoperiod %>%
+#   mutate(phototaken = case_when(!is.na(photonumber) ~ 1,
+#                                 is.na(photonumber) ~ 0))
+# 
+# #? I am unsure if the below defintion is correct, because I don't know the data well, 
+# # but these patients all have a Y dor dr. p or Dr. N, so I am assuming this means exam
+# 
+# beforephotoperiod  <- beforephotoperiod %>%
+#   mutate(examperformed = case_when(include == "Y" ~ 1,
+#                                    include == "N" ~ 0))
+# 
+# ## Returning to data:
+# # my confusion is that it seems like "patientrefertochoeng" would be the right way to do this,
+# # however this variable is missing for most cases in the before, 
+# # maybe this is the point and I will define separately as refertochoeng
+# 
+# beforephotoperiod  <- beforephotoperiod %>%
+#   mutate(refertochoeng = case_when(patientrefertochoeng == "Yes" ~ 1,
+#                                    TRUE ~ 0))
+# 
+# ##create column if CD4 is <100
+# 
+# afterphotoperiod  <- afterphotoperiod %>%
+#   mutate(cd4less100 = case_when(cd4_cell_ws <100 ~ 1,
+#                                 cd4_cell_ws >100 ~ 0))
+# 
+# beforephotoperiod  <- beforephotoperiod %>%
+#   mutate(cd4less100 = case_when(cd4_cell_ws <100 ~ 1,
+#                                 cd4_cell_ws >100 ~ 0))
+# 
+# afterphotoperiod  <- afterphotoperiod %>%
+#   mutate(cd4less100.2 = case_when(cd4count <100 ~ 1,
+#                                   cd4count >100 ~ 0))
+# 
+# beforephotoperiod  <- beforephotoperiod %>%
+#   mutate(cd4less100.2 = case_when(cd4count <100 ~ 1,
+#                                   cd4count >100 ~ 0))
+# 
+# ## Tally numbers in each group and divide by total 
+# 
+# count(beforephotoperiod, examperformed)
+# #348/486
+# count(afterphotoperiod, phototaken)
+# #228/595
+# count(beforephotoperiod, cd4less100)
+# #10/486
+# count(afterphotoperiod, cd4less100)
+# #21/595
+# count(beforephotoperiod, cd4less100.2)
+# #2/486 with 433 NA
+# count(afterphotoperiod, cd4less100.2)
+# #6/595 with 371 NA
+# 
+# ### Hmm, to my understanding this is the opposite of our hypothesis, correct?
+# # I feel that I may not have made the definitions. Are these correct? 
+# # I may stop here to clarify before moving forward...
+# # Also the amount of participants with a cd4 count <100 is way smaller than what is listed in the .do file
+# ### Returning to data:
+# count(beforephotoperiod, refertochoeng)
+# #8/486
+# 
+# ## Ok, perhaps this is the real number ~2% before camera to ~33% after camera, this seems right now, correct?
+# ##Let's look at % with cd4 <100
+# 
+# beforecd4less100 <- beforephotoperiod %>%
+#   filter(cd4less100 ==1)
+# 
+# count(beforecd4less100, refertochoeng)
+# # 0/10
+# 
+# aftercd4less100 <- afterphotoperiod %>%
+#   filter(cd4less100 ==1)
+# 
+# count(aftercd4less100, phototaken)
+# # 7/21
+# 
+# #From 0 to 33% in this group as well. 
